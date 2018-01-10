cee70e4fde7a32db14980ac2f5d6fba17392ef95
Kyle Thomson <kjay.thomson@gmail.com>
2018-01-10 20:19:03 +1100

    Don't require default locale key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const miniTrans = {
   messages: {},
   locale: "en",
-  defaultLocale: "en",
 }
 
 const getTranslation = (msgId) => {
-  if(miniTrans.locale === miniTrans.defaultLocale) return msgId
+  if(miniTrans.locale === "en") return msgId
   const msg = miniTrans.messages[miniTrans.locale][msgId]
   if(msg) return msg
-  const defaultLanguageMsg = miniTrans.messages.en[msgId]
-  if(defaultLanguageMsg) return defaultLanguageMsg
   return ""
 }
 
