466e6066fc92918005ce4d36f63796778385e16c
Kyle Thomson <kjay.thomson@gmail.com>
2018-05-12 16:21:30 +1000

    Use instances to control scope

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-const miniTrans = {
-  messages: {},
-  locale: "en",
+function MiniTrans(messages = {}, locale = "en") {
+  this.messages = messages;
+  this.locale = locale;
+  this.getTranslation = function(msgId) {
+    if (locale === "en") return msgId;
+    let msg = messages[locale][msgId];
+    if (msg) return msg;
+    else return msgId;
+    return "";
+  };
 }
 
-const getTranslation = (msgId) => {
-  if(miniTrans.locale === "en") return msgId
-  const msg = miniTrans.messages[miniTrans.locale][msgId]
-  if(msg) return msg
-  else return msgId
-  return ""
+function makeTranslations(messages, locale) {
+  let miniTrans = new MiniTrans(messages, locale);
+  return miniTrans.getTranslation;
 }
 
-const makeTranslations = (messages, locale) => { 
-  miniTrans.messages = messages
-  miniTrans.locale = locale
-  return getTranslation
-};
-
-export { makeTranslations }
+export { makeTranslations };
